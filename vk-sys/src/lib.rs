bbd2c5e552a6037a8332dc565dd7a77bd3ad03fd
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-11-03 11:41:32 -0300

    vk-sys/src/lib.rs: macros for flag/id definitions

--- vk-sys/src/lib.rs
+++ vk-sys/src/lib.rs
@@ -25,18 +25,44 @@ macro_rules! def_ndh {
     };
 }
 
+// Defines 32-bit flags an their associated types.
+macro_rules! def_flags {
+    ($mask:ident, $bit:ident, $( $cons:ident = $val:expr ),*) => {
+        pub type $mask = u32;
+        pub type $bit = u32;
+        $( pub const $cons: $bit = $val; )*
+    };
+}
+
+// Defines 64-bit flags and their associated types.
+macro_rules! def_flags64 {
+    ($mask:ident, $bit:ident, $( $cons:ident = $val:expr ),*) => {
+        pub type $mask = u64;
+        pub type $bit = u64;
+        $( pub const $cons: $bit = $val; )*
+    };
+}
+
+// Defines IDs (non-flags) and their associated type.
+macro_rules! def_ids {
+    ($name:ident, $( $cons:ident = $val:expr ),*) => {
+        pub type $name = core::ffi::c_int;
+        $( pub const $cons: $name = $val; )*
+    };
+}
+
 mod core;
 pub use crate::core::*;
 
 /// VK_NULL_HANDLE
-/// TODO: Pointer instead in 64-bit archs.
+// TODO: Pointer instead in 64-bit archs.
 #[inline]
 pub fn null_handle() -> u64 {
     0
 }
 
 /// Checks whether `ndh` is equal to `null_handle()`.
-/// TODO: Pointer instead in 64-bit archs.
+// TODO: Pointer instead in 64-bit archs.
 #[inline]
 pub fn is_null_handle(ndh: u64) -> bool {
     ndh == 0
