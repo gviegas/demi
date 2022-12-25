e7810cf8b8a65ce7a3f407b86b0395d6416b3942
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-12-25 17:27:20 -0300

    src/gpu.rs: changes to initialization

--- src/gpu.rs
+++ src/gpu.rs
@@ -2,20 +2,26 @@
 
 //! Interface to the graphics back-end.
 
+use std::sync::Once;
+
 #[cfg(test)]
 mod tests;
 
 mod vk;
 
+static mut IMPL: Option<Box<dyn Gpu>> = None;
+
 /// Initializes the underlying `Gpu` implementation.
-pub fn init() -> Option<Box<dyn Gpu>> {
-    // TODO: This won't compile on platforms where
-    // `vk_sys` isn't supported.
-    if cfg!(any(target_os = "linux", windows)) {
-        vk::init()
-    } else {
-        None
-    }
+///
+/// Panics if all back-ends fail to initialize.
+#[cfg(any(target_os = "linux", windows))]
+pub fn init() {
+    static INIT: Once = Once::new();
+    INIT.call_once(|| unsafe {
+        IMPL = vk::init();
+        IMPL.is_none()
+            .then(|| panic!("no graphics back-end that we can use"));
+    });
 }
 
 /// Graphics back-end interface.
--- src/gpu/tests.rs
+++ src/gpu/tests.rs
@@ -1,8 +1,16 @@
 // Copyright 2022 Gustavo C. Viegas. All rights reserved.
 
+use std::thread;
+
 use crate::gpu;
 
 #[test]
 fn init() {
-    let _ = gpu::init();
+    let mut join = vec![];
+    for _ in 0..5 {
+        join.push(thread::spawn(gpu::init));
+    }
+    for i in join {
+        i.join().unwrap();
+    }
 }
