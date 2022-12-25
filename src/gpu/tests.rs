0a825a3fc6a8317fa3699ae05bdc6926b949e900
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-12-25 18:39:10 -0300

    src/gpu/tests.rs: missing assertions

--- src/gpu/tests.rs
+++ src/gpu/tests.rs
@@ -2,10 +2,11 @@
 
 use std::thread;
 
-use crate::gpu;
+use crate::gpu::{self, IMPL};
 
 #[test]
 fn init() {
+    assert!(unsafe { IMPL.is_none() });
     let mut join = vec![];
     for _ in 0..5 {
         join.push(thread::spawn(gpu::init));
@@ -13,5 +14,7 @@ fn init() {
     for i in join {
         i.join().unwrap();
     }
+    assert!(unsafe { IMPL.is_some() });
     gpu::shutdown();
+    assert!(unsafe { IMPL.is_none() });
 }
