3345f2cba0b268bbd518581d5d94aa11d1365d54
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-12-28 00:23:37 -0300

    src/lib.rs: init and shutdown

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 // Copyright 2022 Gustavo C. Viegas. All rights reserved.
 
+use std::hint;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 pub mod animation;
 pub mod drawable;
 pub mod light;
@@ -15,3 +18,64 @@ pub mod texture;
 pub mod transform;
 
 mod gpu;
+
+// NOTE: The main purpose of making this reference-counted
+// is for parallel testing. Crate users are expected to
+// pair one call to `init` with one call to `shutdown`.
+static RC: AtomicUsize = AtomicUsize::new(0);
+
+/// Initializes the crate for use.
+///
+/// Panics if initialization of any sub-module fails.
+///
+/// NOTE: The crate must not be used until this function completes.
+pub fn init() {
+    match RC.compare_exchange(0, 1, Ordering::SeqCst, Ordering::SeqCst) {
+        Ok(0) => {
+            // NOTE: Initialization of global data must
+            // be done here.
+            gpu::init();
+            RC.store(2, Ordering::SeqCst);
+        }
+        Err(1) => loop {
+            match RC.load(Ordering::SeqCst) {
+                0 => return init(),
+                1 => (),
+                _ => {
+                    RC.fetch_add(1, Ordering::SeqCst);
+                    break;
+                }
+            }
+            hint::spin_loop();
+        },
+        _ => {
+            RC.fetch_add(1, Ordering::SeqCst);
+        }
+    }
+}
+
+/// Finalizes the crate.
+///
+/// NOTE: The crate must not be used afterwards.
+pub fn shutdown() {
+    match RC.compare_exchange(2, 1, Ordering::SeqCst, Ordering::SeqCst) {
+        Ok(2) => {
+            // NOTE: Finalization of global data must
+            // be done here.
+            gpu::shutdown();
+            RC.store(0, Ordering::SeqCst);
+        }
+        Err(1) => loop {
+            match RC.load(Ordering::SeqCst) {
+                0 => break,
+                1 => (),
+                _ => return shutdown(),
+            }
+            hint::spin_loop();
+        },
+        Err(0) => (),
+        _ => {
+            RC.fetch_sub(1, Ordering::SeqCst);
+        }
+    }
+}
