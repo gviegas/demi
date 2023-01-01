6b36edef5e002359c2b44791054a9d1dbf05b85f
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-01-01 11:59:47 -0300

    src/sampler.rs: Builder impl

--- src/sampler.rs
+++ src/sampler.rs
@@ -2,6 +2,8 @@
 
 use std::io;
 
+use crate::gpu::SplrOptions;
+
 /// Sampler.
 pub struct Sampler {
     // TODO
@@ -37,33 +39,63 @@ pub enum Compare {
 
 /// Sampler builder.
 pub struct Builder {
-    // TODO
+    options: SplrOptions,
 }
 
 #[allow(unused_variables)] // TODO
 impl Builder {
+    /// Creates a new sampler builder.
     pub fn new() -> Self {
-        todo!();
+        Self {
+            options: SplrOptions {
+                u_wrap: Wrap::Repeat,
+                v_wrap: Wrap::Repeat,
+                w_wrap: Wrap::Repeat,
+                mag_filter: Filter::Nearest,
+                min_filter: (Filter::Nearest, Some(Filter::Nearest)),
+                compare: None,
+            },
+        }
     }
 
+    /// Sets the wrapping mode of the `u` coordinates.
+    ///
+    /// This value need not be set. It defaults to [`Wrap::Repeat`].
     pub fn set_u_wrap(&mut self, wrap: Wrap) -> &mut Self {
-        todo!();
+        self.options.u_wrap = wrap;
+        self
     }
 
+    /// Sets the wrapping mode of the `v` coordinates.
+    ///
+    /// This value need not be set. It defaults to [`Wrap::Repeat`].
     pub fn set_v_wrap(&mut self, wrap: Wrap) -> &mut Self {
-        todo!();
+        self.options.v_wrap = wrap;
+        self
     }
 
+    /// Sets the wrapping mode of the `w` coordinates.
+    ///
+    /// This value need not be set. It defaults to [`Wrap::Repeat`].
     pub fn set_w_wrap(&mut self, wrap: Wrap) -> &mut Self {
-        todo!();
+        self.options.w_wrap = wrap;
+        self
     }
 
+    /// Sets the magnification filter.
+    ///
+    /// This value need not be set. It defaults to [`Filter::Nearest`].
     pub fn set_mag_filter(&mut self, filter: Filter) -> &mut Self {
-        todo!();
+        self.options.mag_filter = filter;
+        self
     }
 
+    /// Sets the minification filter.
+    ///
+    /// This value need not be set. It defaults to [`Filter::Nearest`].
     pub fn set_min_filter(&mut self, filter: Filter, mipmap: Option<Filter>) -> &mut Self {
-        todo!();
+        self.options.min_filter = (filter, mipmap);
+        self
     }
 
     pub fn create(&mut self) -> io::Result<Sampler> {
