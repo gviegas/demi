56739f0fa4b8eff32686d4f2fb089d9e752ced57
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-02-18 07:27:15 -0300

    src/node.rs: implementation of Graph::insert method

--- src/node.rs
+++ src/node.rs
@@ -79,8 +79,81 @@ impl Graph {
         }
     }
 
+    /// Inserts `node` as descendant of `prev`.
+    /// If `prev` is `None`, then `node` is inserted in the graph
+    /// unconnected.
+    /// It returns a [`NodeId`] that identifies `node` in this
+    /// specific graph.
     pub fn insert(&mut self, node: Node, prev: Option<NodeId>) -> NodeId {
-        todo!();
+        let idx = self
+            .nbits
+            .find()
+            .or_else(|| {
+                self.nodes
+                    .resize_with(self.nodes.len() + NBITS_GRAN, || NodeLink {
+                        next: None,
+                        prev: None,
+                        sub: None,
+                        data: usize::MAX,
+                    });
+                self.nbits.grow(1)
+            })
+            .unwrap();
+        self.nbits.set(idx);
+
+        if let Some(NodeId { node: prev, .. }) = prev {
+            if let Some(sub) = self.nodes[prev].sub {
+                self.nodes[idx].next = Some(sub);
+                self.nodes[sub].prev = Some(idx);
+            } else {
+                self.nodes[idx].next = None;
+            }
+            self.nodes[prev].sub = Some(idx);
+            self.nodes[idx].prev = Some(prev);
+        } else {
+            self.nodes[idx].next = None;
+            self.nodes[idx].prev = None;
+        }
+        self.nodes[idx].sub = None;
+
+        let (typ, data) = match node {
+            Node::Drawable(d, x) => {
+                self.drawables.push(NodeData {
+                    local: x,
+                    world: Default::default(),
+                    changed: true,
+                    hidden: false,
+                    node: idx,
+                    data: d,
+                });
+                (NodeType::Drawable, self.drawables.len() - 1)
+            }
+            Node::Light(l, x) => {
+                self.lights.push(NodeData {
+                    local: x,
+                    world: Default::default(),
+                    changed: true,
+                    hidden: false,
+                    node: idx,
+                    data: l,
+                });
+                (NodeType::Light, self.lights.len() - 1)
+            }
+            Node::Xform(x) => {
+                self.xforms.push(NodeData {
+                    local: x,
+                    world: Default::default(),
+                    changed: true,
+                    hidden: false,
+                    node: idx,
+                    data: (),
+                });
+                (NodeType::Xform, self.xforms.len() - 1)
+            }
+        };
+        self.nodes[idx].data = data;
+
+        NodeId { typ, node: idx }
     }
 
     pub fn remove(&mut self, node: NodeId) -> Node {
