9fa95753f88a6ebc44c08bcbb4c2104df63eab22
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-05-02 13:56:08 -0300

    src/drawable.rs: tests

--- src/drawable.rs
+++ src/drawable.rs
@@ -18,7 +18,7 @@ pub struct Drawable {
 }
 
 /// Shape of a `Drawable`.
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Shape {
     Bbox(Bbox),
     Sphere(Sphere),
@@ -58,5 +58,76 @@ impl Drawable {
     /// or `None` if the drawable has no skin.
     pub fn skin(&self) -> Option<&Arc<Skin>> {
         self.skin.as_ref()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::linear::{Mat4, Vec3};
+    use crate::mesh::{self, DataType, Semantic, Topology};
+    use crate::skin;
+    use std::io;
+
+    impl Drawable {
+        fn check(&self, mesh: &Mesh, shape: &Shape, skin: Option<&Skin>) {
+            assert_eq!(Arc::as_ptr(&self.mesh), mesh);
+            assert_eq!(self.shape, *shape);
+            self.skin.as_ref().map_or_else(
+                || assert!(skin.is_none()),
+                |x| assert_eq!(Arc::as_ptr(x), skin.unwrap()),
+            );
+        }
+    }
+
+    fn make_mesh() -> Mesh {
+        crate::init();
+        mesh::Builder::new()
+            .set_vertex_count(3)
+            .set_semantic(io::repeat(1), Semantic::Position, DataType::F32x3, 12)
+            .unwrap()
+            .push_primitive(Topology::Triangle)
+            .unwrap()
+            .create()
+            .unwrap()
+    }
+
+    fn make_skin() -> Skin {
+        skin::Builder::new()
+            .push_joints(&["Joint"], &[Mat4::from(1.0)], &[None], &[None])
+            .unwrap()
+            .create()
+            .unwrap()
+    }
+
+    #[test]
+    fn no_skin() {
+        let mesh = Arc::new(make_mesh());
+        let bbox = Shape::Bbox(Bbox::new(Vec3::from(0.0), Vec3::from(1.0)));
+        let sphere = Shape::Sphere(Sphere::new(Vec3::from(0.0), 1.0));
+
+        let d1 = Drawable::new(Arc::clone(&mesh), bbox);
+        let d2 = Drawable::new(Arc::clone(&mesh), sphere);
+        let d3 = Drawable::new(Arc::clone(&mesh), Shape::None);
+
+        d1.check(&mesh, &bbox, None);
+        d2.check(&mesh, &sphere, None);
+        d3.check(&mesh, &Shape::None, None);
+    }
+
+    #[test]
+    fn skinned() {
+        let mesh = Arc::new(make_mesh());
+        let skin = Arc::new(make_skin());
+        let bbox = Shape::Bbox(Bbox::new(Vec3::from(0.0), Vec3::from(1.0)));
+        let sphere = Shape::Sphere(Sphere::new(Vec3::from(0.0), 1.0));
+
+        let d1 = Drawable::new_skinned(Arc::clone(&mesh), bbox, Arc::clone(&skin));
+        let d2 = Drawable::new_skinned(Arc::clone(&mesh), sphere, Arc::clone(&skin));
+        let d3 = Drawable::new_skinned(Arc::clone(&mesh), Shape::None, Arc::clone(&skin));
+
+        d1.check(&mesh, &bbox, Some(&skin));
+        d2.check(&mesh, &sphere, Some(&skin));
+        d3.check(&mesh, &Shape::None, Some(&skin));
     }
 }
