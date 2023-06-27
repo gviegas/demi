b6bd32cc9b48aa68f56afc0f21a9d9266027b8fa
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-06-27 15:47:16 -0300

    src/command.rs: add render pass encoder

--- xgpu/src/command.rs
+++ xgpu/src/command.rs
@@ -2,7 +2,10 @@
 
 use std::ops::RangeBounds;
 
-use crate::{BindGroup, Buffer, ComputePipeline, Extent3d, QuerySet, Result};
+use crate::{
+    BindGroup, Buffer, ComputePipeline, Extent3d, IndexFormat, QuerySet, RenderPipeline, Result,
+    TextureView,
+};
 
 pub struct CommandBuffer {
     // TODO
@@ -142,8 +145,168 @@ pub struct ComputePassTimestampWrites<'a> {
     pub end_of_pass_write_index: u32,
 }
 
+pub struct RenderPassEncoder {
+    // TODO
+}
+
+impl RenderPassEncoder {
+    pub fn set_viewport(
+        &mut self,
+        _x: f32,
+        _y: f32,
+        _width: f32,
+        _height: f32,
+        _min_depth: f32,
+        _max_depth: f32,
+    ) {
+        panic!("not yet implemented");
+    }
+
+    pub fn set_scissor_rect(&mut self, _x: u32, _y: u32, _width: u32, _height: u32) {
+        panic!("not yet implemented");
+    }
+
+    pub fn set_blend_constant(&mut self, _color: Color) {
+        panic!("not yet implemented");
+    }
+
+    pub fn set_stencil_reference(&mut self, _reference: u32) {
+        panic!("not yet implemented");
+    }
+
+    pub fn begin_occlusion_query(&mut self, _query_index: u32) {
+        panic!("not yet implemented");
+    }
+
+    pub fn end_occlusion_query(&mut self) {
+        panic!("not yet implemented");
+    }
+
+    pub fn execute_bundles(&mut self, _bundles: &[&RenderBundle]) {
+        panic!("not yet implemented");
+    }
+
+    pub fn set_bind_group(
+        &mut self,
+        _index: u32,
+        _bind_group: Option<&BindGroup>,
+        _dynamic_offsets: &[u32],
+    ) {
+        panic!("not yet implemented");
+    }
+
+    pub fn set_pipeline(&mut self, _pipeline: &RenderPipeline) {
+        panic!("not yet implemented");
+    }
+
+    pub fn set_index_buffer(
+        &mut self,
+        _buffer: &Buffer,
+        _index_format: IndexFormat,
+        _range: impl RangeBounds<u64>,
+    ) {
+        panic!("not yet implemented");
+    }
+
+    pub fn set_vertex_buffer(
+        &mut self,
+        _slot: u32,
+        _buffer: &Buffer,
+        _range: impl RangeBounds<u64>,
+    ) {
+        panic!("not yet implemented");
+    }
+
+    pub fn draw(
+        &mut self,
+        _vertex_count: u32,
+        _instance_count: u32,
+        _first_vertex: u32,
+        _first_instance: u32,
+    ) {
+        panic!("not yet implemented");
+    }
+
+    pub fn draw_indexed(
+        &mut self,
+        _index_count: u32,
+        _instance_count: u32,
+        _first_index: u32,
+        _base_vertex: i32,
+        _first_instance: u32,
+    ) {
+        panic!("not yet implemented");
+    }
+
+    pub fn draw_indirect(&mut self, _indirect_buffer: &Buffer, _indirect_offset: u64) {
+        panic!("not yet implemented");
+    }
+
+    pub fn draw_indexed_indirect(&mut self, _indirect_buffer: &Buffer, _indirect_offset: u64) {
+        panic!("not yet implemented");
+    }
+
+    pub fn end(self) {
+        panic!("not yet implemented");
+    }
+
+    // TODO: Debug markers.
+}
+
+pub struct RenderPassDescriptor<'a, 'b, 'c, 'd, 'e> {
+    pub color_attachments: Vec<RenderPassColorAttachment<'a, 'b>>,
+    pub depth_stencil_attachment: Option<RenderPassDepthStencilAttachment<'c>>,
+    pub occlusion_query_set: &'d QuerySet,
+    pub timestamp_writes: RenderPassTimestampWrites<'e>,
+    pub max_draw_count: u64,
+}
+
+pub struct RenderPassColorAttachment<'a, 'b> {
+    pub view: &'a TextureView,
+    pub resolve_target: Option<&'b TextureView>,
+    pub clear_value: Color,
+    pub load_op: LoadOp,
+    pub store_op: StoreOp,
+}
+
+pub struct RenderPassDepthStencilAttachment<'a> {
+    pub view: &'a TextureView,
+    pub depth_clear_value: f32,
+    pub depth_load_op: LoadOp,
+    pub depth_store_op: StoreOp,
+    pub depth_read_only: bool,
+    pub stencil_clear_value: u32,
+    pub stencil_load_op: LoadOp,
+    pub stencil_store_op: StoreOp,
+    pub stencil_read_only: bool,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum LoadOp {
+    Load,
+    Clear,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum StoreOp {
+    Store,
+    Discard,
+}
+
+pub struct RenderPassTimestampWrites<'a> {
+    pub query_set: &'a QuerySet,
+    pub beginning_of_pass_write_index: u32,
+    pub end_of_pass_write_index: u32,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum Color {
+    Float(f32, f32, f32, f32),
+    Sint(i32, i32, i32, i32),
+    Uint(u32, u32, u32, u32),
+}
+
 // TODO
-pub struct RenderPassEncoder;
-pub struct RenderPassDescriptor;
+pub struct RenderBundle;
 pub struct ImageCopyBuffer;
 pub struct ImageCopyTexture;
