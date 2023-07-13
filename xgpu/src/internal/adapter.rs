27fb939771413596ac6b817626f00b1b40bc7b97
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-07-13 10:45:44 -0300

    add new function & inner field to internal::Adapter

--- xgpu/src/internal/adapter.rs
+++ xgpu/src/internal/adapter.rs
@@ -1,5 +1,15 @@
+use crate::{internal, RequestAdapterOptions, Result};
+
 pub struct Adapter {
-    // TODO
+    inner: Box<dyn NAdapter>,
+}
+
+impl Adapter {
+    pub fn new(options: &RequestAdapterOptions) -> Result<Self> {
+        Ok(Self {
+            inner: internal::new_nadapter(options)?,
+        })
+    }
 }
 
 pub(super) trait NAdapter {
