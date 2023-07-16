f62f4962ac2faf3bf1ee10ed1a0724cdf7f4eae8
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-07-16 11:02:34 -0300

    implement Drop trait for vk::Instance

--- platform/vk/src/instance.rs
+++ platform/vk/src/instance.rs
@@ -1,27 +1,28 @@
 use std::ptr;
+use vk_sys as vks;
 
 /// Instance.
 pub struct Instance {
-    inst: vk_sys::Instance,
-    fp: vk_sys::InstanceFp,
+    inst: vks::Instance,
+    fp: vks::InstanceFp,
 }
 
 impl Instance {
     pub fn new(/*_exts: &[&str]*/) -> Result<Self, &'static str> {
-        vk_sys::init()?;
+        vks::init()?;
 
-        let app_info = vk_sys::ApplicationInfo {
-            s_type: vk_sys::STRUCTURE_TYPE_APPLICATION_INFO,
+        let app_info = vks::ApplicationInfo {
+            s_type: vks::STRUCTURE_TYPE_APPLICATION_INFO,
             next: ptr::null(),
             application_name: ptr::null(),
             application_version: 0,
             engine_name: ptr::null(),
             engine_version: 0,
-            api_version: vk_sys::API_VERSION_1_0,
+            api_version: vks::API_VERSION_1_0,
         };
 
-        let inst_info = vk_sys::InstanceCreateInfo {
-            s_type: vk_sys::STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        let inst_info = vks::InstanceCreateInfo {
+            s_type: vks::STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
             next: ptr::null(),
             flags: 0,
             application_info: &app_info,
@@ -34,16 +35,23 @@ impl Instance {
 
         unsafe {
             let mut inst = ptr::null_mut();
-            match vk_sys::create_instance(&inst_info, ptr::null(), &mut inst) {
-                vk_sys::SUCCESS => match vk_sys::InstanceFp::new(inst) {
+            match vks::create_instance(&inst_info, ptr::null(), &mut inst) {
+                vks::SUCCESS => match vks::InstanceFp::new(inst) {
                     Ok(fp) => Ok(Self { inst, fp }),
                     Err(_) => {
-                        vk_sys::fini();
+                        vks::fini();
                         Err("vk_sys::InstanceFp::new failed")
                     }
                 },
                 _ => Err("vk_sys::create_instance failed"),
             }
         }
+    }
+}
+
+impl Drop for Instance {
+    fn drop(&mut self) {
+        unsafe { self.fp.destroy_instance(self.inst, ptr::null()) };
+        vks::fini();
     }
 }
